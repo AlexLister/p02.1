f9b3c37314eab1fdc97729a649d1ab62bb070b00
alaxander lister <alister18@VenerableBede.schools.sunderland.gov.uk>
2016-09-22 09:55:02 +0100

    Solved all problems

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,4 @@
+
 """
 Problem:
 
@@ -32,3 +33,20 @@ def run_tests():
 
 # Edit this function
 def fizzbuzz(n):
+
+    if n % 5 == 0 and n % 3 == 0:
+        print ("fizzbuzz")
+        
+    elif n % 5 == 0:
+        print ("buzz")
+
+    elif n % 3 == 0:
+        print ("fizz")
+
+
+    else:
+        print (n)
+
+    
+
+#COMPLETE
